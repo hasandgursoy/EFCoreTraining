71269c4515d72e4602aac018bc66897670bd5c57
hasandgursoy <hasan.d.gursoy@gmail.com>
2023-08-07 23:06:56 +0300

    Added : Sequence

---
+++ Sequence/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+ApplicationDbContext context = new();
+
+#region Sequence Nedir?
+//Veritabanında benzersiz ve ardışık sayısal değerler üreten veritabanı nesnesidir.
+//Sequence herhangi bir tablonun özelliği değildir. Veritabanı nesnesidir. Birden fazla tablo tarafından kullanılabilir.
+#endregion
+#region Sequence Tanımlama
+//Sequence'ler üzerinden değer oluştururken veritabanına özgü çalışma yapılması zaruridir. SQL Server'a özel yazılan Sequence tanımı misal olarak Oracle için hata verebilir.
+
+#region HasSequence
+
+#endregion
+#region HasDefaultValueSql
+
+#endregion
+#endregion
+
+//await context.Employees.AddAsync(new() { Name = "Gençay", Surname = "Yıldız", Salary = 1000 });
+//await context.Employees.AddAsync(new() { Name = "Mustafa", Surname = "Yıldız", Salary = 1000 });
+//await context.Employees.AddAsync(new() { Name = "Tuaip", Surname = "Yıldız", Salary = 1000 });
+
+//await context.Customers.AddAsync(new() { Name = "Muiddin" });
+//await context.SaveChangesAsync();
+
+#region Sequence Yapılandırması
+
+#region StartsAt
+
+#endregion
+#region IncrementsBy
+
+#endregion
+#endregion
+#region Sequence İle Identity Farkı
+//Sequence bir veritabanı nesnesiyken, Identity ise tabloların özellikleridir.
+//Yani Sequence herhangi bir tabloya bağımlı değildir. 
+//Identity bir sonraki değeri diskten alırken Sequence ise RAM'den alır. Bu yüzden önemli ölçüde Identity'e nazaran daha hızlı, performanslı ve az maliyetlidir.
+#endregion
+
+class Employee
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public int Salary { get; set; }
+}
+class Customer
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasSequence("EC_Sequence")
+            .StartsAt(100)
+            .IncrementsBy(5);
+
+
+        // Her iki tabloda tek bir seuqence kullanır. Bundan dolayı id değerleri iki tabloda da eklendiği sıraya göre işlenecek 4. index de ki değeri gider customer tablosuna id si 4 olacak şekilde yazar.
+        modelBuilder.Entity<Employee>()
+            .Property(e => e.Id)
+            .HasDefaultValueSql("NEXT VALUE FOR EC_Sequence");
+
+        modelBuilder.Entity<Customer>()
+            .Property(c => c.Id)
+            .HasDefaultValueSql("NEXT VALUE FOR EC_Sequence");
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Server=127.0.0.1;Port=5432;Database=ETicaretDb;User Id=postgres;Password=postgres;");
+    }
+}
