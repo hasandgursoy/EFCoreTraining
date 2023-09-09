9e611d86fdd2421a797efb34f1f2e7cc4f687947
hasandgursoy <hasan.d.gursoy@gmail.com>
2023-09-09 15:24:46 +0300

    Added : Entity Splitting

--- EF_Core_Efficient_Querying/Program.cs
+++ EF_Core_Efficient_Querying/Program.cs
@@ -124,7 +124,7 @@ class ApplicationDbContext : DbContext
     }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql("Server=127.0.0.1;Port=5432;Database=DenemeDB;User Id=postgres;Password=123456;")
-            .UseLazyLoadingProxies();
+        optionsBuilder.UseNpgsql("Server=127.0.0.1;Port=5432;Database=DenemeDB;User Id=postgres;Password=123456;");
+            // .UseLazyLoadingProxies();
     }
 }
